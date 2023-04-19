b4061954daf99cb89e09753ac13eb4ca25340a2d
ericketoo <egacgamer@gmail.com>
2023-04-19 10:28:28 -0300

    fist commit

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+const lenguage = document.getElementById('lenguage')
+const Close = document.getElementById ('close')
+
+Close.addEventListener('click', () => {
+    lenguage.style.display = 'none'
+})
